ccf526173f15cb8f2c62f251807bc25b52a9328b
Takuma Matsushita <takuma.matt@gmail.com>
2021-08-01 01:56:52 +0900

    Remove mermalade from package-archives

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
